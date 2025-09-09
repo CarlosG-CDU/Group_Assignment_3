7b62bcc15280c1465fca14fc629a2b81de31d6ea
Carlos Galli <carlos.galli@students.cdu.edu.au>
2025-09-10 09:01:01 +1000

    Added GUI Test

---
+++ GUI.py
@@ -0,0 +1,11 @@
+#using class example to learn...
+
+import tkinter as tk
+
+root = tk.Tk()  #creates a blank window
+root.title("Group Assn 3")
+
+theLabel = tk.Label(root, text = "welcome") #creates a lable widget
+theLabel.pack() #put inside window
+
+root.mainloop() #keep window open
