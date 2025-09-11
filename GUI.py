661fe09d74c0da336bbdbb64dd637b026df9fe3f
Carlos Galli <carlos.galli@students.cdu.edu.au>
2025-09-11 13:30:41 +1000

    added main loop

--- GUI.py
+++ GUI.py
@@ -1,11 +1,13 @@
-#using class example to learn...
+#GUI page to handle UI
 
 import tkinter as tk
 
-root = tk.Tk()  #creates a blank window
-root.title("Group Assn 3")
-
-theLabel = tk.Label(root, text = "welcome") #creates a lable widget
-theLabel.pack() #put inside window
-
-root.mainloop() #keep window open
+class HugFaceGui:
+    def__init__(self, root):
+        self._root = root   #this is encapsulation      
+        self._root.title("Group 5 Assingment 3")
+        self.setup_layout()
+    
+    def setup_layout(self):
+        label = tk.Label(self._root,, text = "Welcome to Assignment 3")
+        label.pack(padx = 10, pady = 10)
