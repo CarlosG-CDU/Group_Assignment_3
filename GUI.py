6eff0bed582dd4ab0f550878e8d4b351b87dd161
Carlos Galli <carlos.galli@students.cdu.edu.au>
2025-09-11 15:47:58 +1000

    fixed typo errors

--- GUI.py
+++ GUI.py
@@ -3,11 +3,11 @@
 import tkinter as tk
 
 class HugFaceGui:
-    def__init__(self, root):
-        self._root = root   #this is encapsulation      
+    def __init__(self, root):
+        self._root = root   # Store the root window (protected attribute by convention)     
         self._root.title("Group 5 Assingment 3")
         self.setup_layout()
     
     def setup_layout(self):
-        label = tk.Label(self._root,, text = "Welcome to Assignment 3")
+        label = tk.Label(self._root, text = "Welcome to Assignment 3")
         label.pack(padx = 10, pady = 10)
