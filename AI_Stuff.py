0b76bc325d7a2afe9c0b197c3363ae409d2cf937
Carlos Galli <carlos.galli@students.cdu.edu.au>
2025-09-13 11:02:19 +1000

    Added AI_Stuff.py to try to get an AI model running

---
+++ AI_Stuff.py
@@ -0,0 +1,17 @@
+#https://huggingface.co/distilbert/distilbert-base-uncased-finetuned-sst-2-english
+
+#example AI sentiment. Got this running but not linked to GUI.py
+
+import torch
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+predicted_class_id = logits.argmax().item()
+model.config.id2label[predicted_class_id]
+print(model.config.id2label[predicted_class_id])
